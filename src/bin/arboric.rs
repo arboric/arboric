d6e51cc69563f4966a75d9a87489e6bd9d7cea71
Alistair A. Israel <aisrael@gmail.com>
2019-06-23 18:42:18 +0800

    Hello, world using hyper

--- src/bin/arboric.rs
+++ src/bin/arboric.rs
@@ -1,8 +1,36 @@
+extern crate hyper;
+
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Request, Response, Server};
+use simplelog::{LevelFilter, SimpleLogger};
 use std::error::Error;
-use simplelog::{SimpleLogger,LevelFilter};
+
+const PHRASE: &str = "Hello, World!";
+
+fn hello_world(_req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from(PHRASE))
+}
 
 fn main() -> Result<(), Box<Error>> {
     let _ = SimpleLogger::init(LevelFilter::Trace, simplelog::Config::default());
+
+    // This is our socket address...
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let new_svc = || {
+        // service_fn_ok converts our function into a `Service`
+        service_fn_ok(hello_world)
+    };
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    // Run this server for... forever!
+    hyper::rt::run(server);
 
     println!("Ok");
     Ok(())
