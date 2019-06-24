ae271bb6c51ae81f98163a86e4c3953197cbe0f8
Alistair A. Israel <aisrael@gmail.com>
2019-06-24 13:25:39 +0800

    Trying to get this to work

--- src/bin/arboric.rs
+++ src/bin/arboric.rs
@@ -1,19 +1,62 @@
 extern crate hyper;
 
-use hyper::rt::Future;
+use hyper::rt::{self, Future};
+
 use hyper::service::service_fn_ok;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Client, Request, Response, Server};
+use log::{info, trace, warn};
 use simplelog::{LevelFilter, SimpleLogger};
 use std::error::Error;
 
 const PHRASE: &str = "Hello, World!";
 
 fn hello_world(_req: Request<Body>) -> Response<Body> {
+    let uri = "http://localhost:4000/graphql?query={tweets{id}}"
+        .parse()
+        .unwrap();
+
+    let client = Client::new();
+
+    let res = client
+        .get(uri)
+        .map(|res| {
+            let s = format!("*** Response: {}", res.status());
+            println!("{}", s);
+            s
+        })
+        .map_err(|err| {
+            let s = format!("*** Error: {}", err);
+            println!("{}", s);
+            err
+        })
+        .wait().unwrap();
+
+    trace!("{}", res);
+
     Response::new(Body::from(PHRASE))
 }
 
 fn main() -> Result<(), Box<Error>> {
-    let _ = SimpleLogger::init(LevelFilter::Trace, simplelog::Config::default());
+    let _ = SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default());
+
+    rt::run(rt::lazy(|| {
+        let uri = "http://localhost:4000/graphql?query={tweets{id}}"
+            .parse()
+            .unwrap();
+
+        let client = Client::new();
+
+        client
+            .get(uri)
+            .map(|res| {
+                let s = format!("*** Response: {}", res.status());
+                println!("{}", s);
+            })
+            .map_err(|err| {
+                let s = format!("*** Error: {}", err);
+                println!("{}", s);
+            })
+    }));
 
     // This is our socket address...
     let addr = ([127, 0, 0, 1], 3000).into();
