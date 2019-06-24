3700e56b4700060f3b2542c23b50910e496801e7
Alistair A. Israel <aisrael@gmail.com>
2019-06-24 21:40:02 +0800

    Super simple working 'proxy'

--- src/bin/arboric.rs
+++ src/bin/arboric.rs
@@ -1,44 +1,26 @@
 extern crate hyper;
 
 use hyper::rt::{self, Future};
-
-use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
 use hyper::{Body, Client, Request, Response, Server};
-use log::{info, trace, warn};
+use log::debug;
 use simplelog::{LevelFilter, SimpleLogger};
 use std::error::Error;
 
-const PHRASE: &str = "Hello, World!";
+// Just a simple type alias
+type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
-fn hello_world(_req: Request<Body>) -> Response<Body> {
+fn proxy(_req: Request<Body>) -> BoxFut {
     let uri = "http://localhost:4000/graphql?query={tweets{id}}"
         .parse()
         .unwrap();
 
     let client = Client::new();
 
-    let res = client
-        .get(uri)
-        .map(|res| {
-            let s = format!("*** Response: {}", res.status());
-            println!("{}", s);
-            s
-        })
-        .map_err(|err| {
-            let s = format!("*** Error: {}", err);
-            println!("{}", s);
-            err
-        })
-        .wait().unwrap();
-
-    trace!("{}", res);
-
-    Response::new(Body::from(PHRASE))
+    Box::new(client.get(uri))
 }
 
-fn main() -> Result<(), Box<Error>> {
-    let _ = SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default());
-
+fn do_get() {
     rt::run(rt::lazy(|| {
         let uri = "http://localhost:4000/graphql?query={tweets{id}}"
             .parse()
@@ -50,26 +32,25 @@ fn main() -> Result<(), Box<Error>> {
             .get(uri)
             .map(|res| {
                 let s = format!("*** Response: {}", res.status());
-                println!("{}", s);
+                debug!("{}", s);
             })
             .map_err(|err| {
                 let s = format!("*** Error: {}", err);
-                println!("{}", s);
+                debug!("{}", s);
             })
     }));
+}
+
+fn main() -> Result<(), Box<Error>> {
+    let _ = SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default());
+
+    do_get();
 
     // This is our socket address...
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let new_svc = || {
-        // service_fn_ok converts our function into a `Service`
-        service_fn_ok(hello_world)
-    };
-
     let server = Server::bind(&addr)
-        .serve(new_svc)
+        .serve(|| service_fn(proxy))
         .map_err(|e| eprintln!("server error: {}", e));
 
     // Run this server for... forever!
