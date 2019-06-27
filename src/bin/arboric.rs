088a9f01554a55358ee49344d989cf297416f757
Alistair A. Israel <aisrael@gmail.com>
2019-06-27 19:13:53 +0800

    Introducing arboric::Proxy

--- src/bin/arboric.rs
+++ src/bin/arboric.rs
@@ -1,91 +1,16 @@
 extern crate hyper;
 
-use futures::future;
-use hyper::rt::{Future, Stream};
-use hyper::service::service_fn;
-use hyper::{Body, Client, Method, Request, Response, Server, StatusCode, Uri};
-use log::{debug, warn};
+use log::debug;
 use simplelog::{LevelFilter, SimpleLogger};
 use std::error::Error;
-
-// Just a simple type alias
-type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
-
-fn do_get(req: Request<Body>) -> BoxFut {
-    let req_uri = req.uri();
-    debug!("req_uri => {}", req_uri);
-
-    let params = req.uri().query().unwrap();
-    let pandq = format!("/graphql?{}", params);
-
-    let uri = Uri::builder()
-        .scheme("http")
-        .authority("localhost:4000")
-        .path_and_query(&pandq[..])
-        .build()
-        .unwrap();
-
-    debug!("uri => {}", uri);
-
-    let client = Client::new();
-    let fut = client
-        .get(uri)
-        .and_then(|res| {
-            debug!("GET /localhost:4000 => {}", res.status());
-            future::ok(res)
-        })
-        .map_err(|err| {
-            warn!("{}", err);
-            err
-        });
-
-    Box::new(fut)
-}
-
-fn do_post(req: Request<Body>) -> BoxFut {
-    let req_uri = req.uri();
-    debug!("req_uri => {}", req_uri);
-
-    let uri: hyper::Uri = "http://localhost:4000/graphql".parse().unwrap();
-    debug!("uri => {}", uri);
-
-    debug!("{:?}", req.body());
-
-    let mut request = Request::post(uri)
-        .header("Content-Type", "application/graphql")
-        .body(Body::empty())
-        .unwrap();
-
-    *request.body_mut() = req.into_body();
-
-    let client = Client::new();
-    Box::new(client.request(request))
-}
-
-fn proxy(req: Request<Body>) -> BoxFut {
-    match req.method() {
-        &Method::GET => do_get(req),
-        &Method::POST => do_post(req),
-        _ => {
-            let mut response = Response::new(Body::empty());
-            *response.status_mut() = StatusCode::NOT_FOUND;
-            Box::new(future::ok(response))
-        }
-    }
-}
 
 fn main() -> Result<(), Box<Error>> {
     let _ = SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default());
 
-    // This is our socket address...
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let proxy = arboric::Proxy{};
+    debug!("{:?}", proxy);
 
-    let server = Server::bind(&addr)
-        .serve(|| service_fn(proxy))
-        .map_err(|e| eprintln!("server error: {}", e));
-
-    // Run this server for... forever!
-    hyper::rt::run(server);
+    proxy.run();
 
     println!("Ok");
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,100 @@
+/// The arboric library
+
+use futures::future;
+use hyper::rt::Future;
+use hyper::service::service_fn;
+use hyper::{Body, Client, Method, Request, Response, Server, StatusCode, Uri};
+use log::{debug, warn};
+
+/// The main Proxy
+#[derive(Debug)]
+pub struct Proxy {}
+
+impl Proxy {
+    pub fn new() -> Proxy {
+        Proxy {}
+    }
+
+    pub fn run(&self) {
+        // This is our socket address...
+        let addr = ([127, 0, 0, 1], 4000).into();
+        let server = Server::bind(&addr)
+            .serve(|| service_fn(proxy_service))
+            .map_err(|e| eprintln!("server error: {}", e));
+
+        // Run this server for... forever!
+        hyper::rt::run(server);
+    }
+}
+
+const API_URI: &str = "http://localhost:3000/graphql";
+
+// Just a simple type alias
+type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+fn do_get(req: Request<Body>) -> BoxFut {
+    let req_uri = req.uri();
+    debug!("req_uri => {}", req_uri);
+
+    let params = req.uri().query().unwrap();
+    let pandq = format!("/graphql?{}", params);
+
+    let uri = Uri::builder()
+        .scheme("http")
+        .authority("localhost:4000")
+        .path_and_query(&pandq[..])
+        .build()
+        .unwrap();
+
+    debug!("uri => {}", uri);
+
+    let client = Client::new();
+    let fut = client
+        .get(uri)
+        .and_then(|res| {
+            debug!("GET /localhost:4000 => {}", res.status());
+            future::ok(res)
+        })
+        .map_err(|err| {
+            warn!("{}", err);
+            err
+        });
+
+    Box::new(fut)
+}
+
+fn do_post(req: Request<Body>) -> BoxFut {
+    let req_uri = req.uri();
+    debug!("req_uri => {}", req_uri);
+
+    let uri: hyper::Uri = API_URI.parse().unwrap();
+    debug!("uri => {}", uri);
+
+    debug!("{:?}", req.body());
+
+    let mut request = Request::post(uri)
+        .header("Content-Type", "application/graphql")
+        .body(Body::empty())
+        .unwrap();
+
+    *request.body_mut() = req.into_body();
+
+    let client = Client::new();
+    Box::new(client.request(request))
+}
+
+fn proxy_service(req: Request<Body>) -> BoxFut {
+    match req.method() {
+        &Method::GET => do_get(req),
+        &Method::POST => do_post(req),
+        _ => {
+            let mut response = Response::new(Body::empty());
+            *response.status_mut() = StatusCode::NOT_FOUND;
+            Box::new(future::ok(response))
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
