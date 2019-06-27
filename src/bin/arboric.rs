2cb42cc7dadbc0f49053b17441e3e568e6c04c54
Alistair A. Israel <aisrael@gmail.com>
2019-06-27 22:13:13 +0800

    Log the thread id

--- src/bin/arboric.rs
+++ src/bin/arboric.rs
@@ -5,7 +5,9 @@ use simplelog::{LevelFilter, SimpleLogger};
 use std::error::Error;
 
 fn main() -> Result<(), Box<Error>> {
-    let _ = SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default());
+    let mut log_config = simplelog::Config::default();
+    log_config.thread = Some(log::Level::Debug);
+    let _ = SimpleLogger::init(LevelFilter::Debug, log_config);
 
     let proxy = arboric::Proxy{};
     debug!("{:?}", proxy);
