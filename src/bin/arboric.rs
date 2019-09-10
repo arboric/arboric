2ad49bca4101eb1614dda64693a6ef9d4e625d34
mikong <michaelgalero@gmail.com>
2019-09-10 20:21:31 +0800

    Use clap to accept CLI arguments

--- src/bin/arboric.rs
+++ src/bin/arboric.rs
@@ -1,13 +1,24 @@
 //! The arboric command line
 extern crate hyper;
 
+#[macro_use]
+extern crate clap;
+
 use log::trace;
 use std::error::Error;
+
+use clap::{App, SubCommand};
 
 const API_URI: &str = "http://localhost:3001/graphql";
 
 /// The `arboric` CLI entrypoint
 fn main() -> Result<(), Box<dyn Error>> {
+    let _matches = App::new("Arboric")
+        .version(crate_version!())
+        .about("GraphQL API Gateway")
+        .subcommand(SubCommand::with_name("start").about("start the arboric server"))
+        .get_matches();
+
     arboric::initialize_logging();
 
     let proxy = arboric::Proxy::new(API_URI);
